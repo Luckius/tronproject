062a2c5fe184410f8036b3ccf8a4cf3a7d1f061f
Luckius <luckiusevodius@gmail.com>
2021-09-02 13:36:15 +0300

    changes

--- src/js/app.js
+++ src/js/app.js
@@ -13,7 +13,7 @@ try {
       metacoinConfig.privateKey
   )
 } catch (err) {
-  alert('The app looks not configured. Please run `npm run migrate`')
+  //alert('The app looks not configured. Please run `npm run migrate`')
 }
 
 //privateKey: "da146374a75310b9666e834ee4ad0866d6f4035967bfc76217c5a495fff9f0d0",
